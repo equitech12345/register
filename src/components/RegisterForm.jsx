6f8946df9a255cf297cd16fd1c947bb423001a1b
Juan Balladarez <juan.balladarez@gmail.com>
2024-06-13 11:07:14 +0100

    Agrego el ingreso de Teléfono al formulario

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -10,6 +10,8 @@ const RegisterForm = memo(() => {
                 <input type="text" name="lastname" id="lastname" />
                 <label >Email</label>
                 <input type="email" name="email" id="email" />
+                <label >Teléfono</label>
+                <input type="tel" name="phone" id="phone" />
                 <label >Contraseña</label>
                 <input type="password" name="password" id="password" />
                 <label >Confirmar contraseña</label>
