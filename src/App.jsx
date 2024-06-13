8884ef718a6f8279fc28128778d305dac6b67c07
Juan Balladarez <juan.balladarez@gmail.com>
2024-06-13 09:51:45 +0100

    Elimino errores y agrego estilos simples

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css'
+import React from 'react'
+import RegisterForm from './RegisterForm'
 
 function App() {
 
   return (
     <>
-     
+    <header>
+      <h1>Registro</h1>
+      <p>Bienvenido, ingrese los datos en el fomulario</p>
+    </header>
+    <RegisterForm />
+       
     </>
   )
 }
